19883c0144e0af9121d11389c9e1cdeb60ea30ff
shon <xggg826@gmail.com>
2022-11-02 16:19:00 +0800

    feat: add basic authentication

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module backup-server
 
 go 1.19
+
+require github.com/abbot/go-http-auth v0.4.0
+
+require (
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,15 +5,30 @@ import (
 	"net/http"
 	"os"
 	"strings"
+
+	auth "github.com/abbot/go-http-auth"
 )
 
+func Secret(user, realm string) string {
+	switch user {
+	case "username": // username
+		return "$1$RzNAxMqf$2Cjw9950B0wEE3CIthyJj." // password  https://unix4lyfe.org/crypt/
+	default:
+		return ""
+	}
+}
+
 func main() {
+	authenticator := auth.NewBasicAuthenticator("agent", Secret)
+
 	port := os.Args[1]
 	for _, path := range os.Args[2:] {
 		fs := http.FileServer(http.Dir(path))
 		parts := strings.Split(path, string(os.PathSeparator))
 		route := "/" + strings.Replace(parts[len(parts)-1], " ", "_", -1)
-		http.Handle(route+"/", http.StripPrefix(route, fs))
+		http.HandleFunc(route+"/", authenticator.Wrap(func(w http.ResponseWriter, r *auth.AuthenticatedRequest) {
+			http.StripPrefix(route, fs).ServeHTTP(w, &r.Request)
+		}))
 	}
 
 	log.Println(http.ListenAndServe(":"+port, nil))
